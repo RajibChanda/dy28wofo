4ca80f28a5ee86dd4a091d8a3e982986792c4500
dy28wofo <rajib.chanda@fau.de>
2021-11-11 20:50:49 +0100

    Updated the code

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from distutils.core import setup
 from setuptools import find_packages
-setup(name='idm_id',
+setup(name='dy28wofo',
 version='0.1',
 author='DSSS',
-author_email='name@fau.de',
+author_email='rajib.chanda@fau.de',
 packages=find_packages(),
 install_requires=['numpy', 'Pillow', 'ipywidgets'])
