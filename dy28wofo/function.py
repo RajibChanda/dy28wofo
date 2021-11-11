405445bc5be828cefa7ae5d743b32591a4aff67e
dy28wofo <rajib.chanda@fau.de>
2021-11-11 21:38:29 +0100

    updated version of the task

--- dy28wofo/function.py
+++ dy28wofo/function.py
@@ -2,10 +2,21 @@ import numpy as np
 from ipywidgets import interact, fixed
 from PIL import Image
 def imshow(X, resize=None):
-"""
-You should create a way to resize an image from an array X.
-The use of widgets is optional but you can take a look to interact.
-We should be able to install this package in Google Colab from your Git
-repo.
-"""
-pass
+    """
+    resizes an image by the given parameter.
+    Returns:
+        Shows a resized image
+    Args:
+        X = Either an image url or a 2D numpy array 
+        resize = a tuple of targetted size, if this is empty then default size would be 512X512
+    """
+    if(resize == None):
+        resize = (512,512)
+    try: 
+        if (str(type(X) == "numpy.ndarray")):
+            X = Image.fromarray(X)
+        #img = Image.open(X)
+        resized_img = X.resize(resize)
+        resized_img.show()
+    except:
+            print("Only accepts numpy array or an image url")
